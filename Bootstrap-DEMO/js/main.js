90eafec8bda6d36b9a11ff6785cb7d7337fdd7bb
khoa-1606 <huynhkhoavo1606@gmail.com>
2023-04-17 15:54:23 +0700

    update

---
+++ Bootstrap-DEMO/js/main.js
@@ -0,0 +1,9 @@
+// alert('connect success')
+$(window).scroll(function () {
+    let oTop = $('.section-2').offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        $('.navbar').addClass('sticky');
+    } else {
+        $('.navbar').removeClass('sticky');
+    }
+})
