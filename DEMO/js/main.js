5c969e741089de858c63e4ec13faef5391c2caf2
khoa-1606 <huynhkhoavo1606@gmail.com>
2023-04-26 21:54:39 +0700

    demo5

--- DEMO/js/main.js
+++ DEMO/js/main.js
@@ -34,3 +34,15 @@ $(window).scroll(function () {
         nCount(".rect > h1")
     }
 })
+
+// search
+const searchWrapper = document.querySelector(".search-container");
+const closeBtn = document.querySelector(".fa-times");
+
+searchWrapper.addEventListener("click", () => {
+    searchWrapper.classList.add("active");
+});
+closeBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    searchWrapper.classList.remove("active");
+});
